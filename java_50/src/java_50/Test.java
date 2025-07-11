208f4e693233d5a587ccdc96063ca0201b3f1a0d
langvietthanh <langvietthanh.28072005@gmail.com>
2025-07-11 12:30:12 +0700

    them phan ghi chu

--- java_50/src/java_50/Test.java
+++ java_50/src/java_50/Test.java
@@ -8,13 +8,13 @@ public class Test {
 		String s1 ,s2;
 		s1 = inp.nextLine();
 		s2 = inp.nextLine();
-		System.out.println(s1.compareTo(s2));
-		System.out.println(s1.compareToIgnoreCase(s2));
-		System.out.println(s1.equals(s2));
-		System.out.println(s1.equalsIgnoreCase(s2));
-		System.out.println(s1.regionMatches(2, s2, 0, 3));
-		String s3 = inp.nextLine();
-		System.out.println(s1.startsWith(s3));
-		System.out.println(s2.endsWith(s3));
+		System.out.println(s1.compareTo(s2));                 // so sanh xau tinh chu in hoa
+		System.out.println(s1.compareToIgnoreCase(s2));		  // so sanh xau khong tinh chu hoa	
+		System.out.println(s1.equals(s2));					  // so sanh bang tinh chu in hoa
+		System.out.println(s1.equalsIgnoreCase(s2));		  // so sanh bang khong tinh chu in hoa
+		System.out.println(s1.regionMatches(2, s2, 0, 3));	  // so sanh xau con 
+		String s3 = inp.nextLine();							  
+		System.out.println(s1.startsWith(s3));				  // so sanh xau bat dau voi xau con
+		System.out.println(s2.endsWith(s3));				  // so sanh xau ket thuc voi xau con
 	}
 }
